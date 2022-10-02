d895af17c8a656b7beff05f21eb74c2b10611bf4
k2supra <andrijkorolevic@gmail.com>
2022-10-02 14:40:44 +0300

    Created new feature/hw_ex_1

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("ABOBA")
+print("Введіть початок діапазону")
+n1 = int(input("->"))
+print("Введіть кінець діапазону")
+n2 = int(input("->"))
+
+for i in range(n1, n2 + 1):
+        if i % 7 == 0:
+            print(i)
