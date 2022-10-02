31571f268b9ac293d1681f4c99d970e6de742622
k2supra <andrijkorolevic@gmail.com>
2022-10-02 16:40:24 +0300

    Created new feature/hw_ex_3

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("ABOBA")
+print("Введіть 1 число")
+n1 = int(input("->"))
+print("Введіть 2 число")
+n2 = int(input("->"))
+
+for i in range(n1, n2+1):
+    if i % 3 == 0:
+        print("Fizz")
+
+    elif i % 5 == 0:
+        print("Buzz")
+
+    elif i % 3 == 0 and i % 5 == 0:
+        print("Fizz Buzz")
+
+else:
+    print(n2 - n1)
